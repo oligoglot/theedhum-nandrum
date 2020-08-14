e4d62ea6dd2d80ec51de912974fcb79d2a11482e
oligoglot <quasilinguist@gmail.com>
2020-08-14 22:54:00 +0530

    added sentiment to all emoji occuring in the data

--- src/playground/emoji_sentiment.py
+++ src/playground/emoji_sentiment.py
@@ -3,6 +3,7 @@ import sys
 import emoji
 import re
 import csv
+from collections import Counter
 # Appeding our src directory to sys path so that we can import modules.
 sys.path.append('../..') 
 
@@ -13,27 +14,33 @@ def extract_emojis(s):
 
 matchedFn = "../../resources/data/matched_emojis.txt"
 unmatchedFn = "../../resources/data/unmatched_emojis.txt"
-matched = []
-unmatched = []
+matched = Counter()
+unmatched = Counter()
+occurences = Counter()
 # Get the list of unmatched emojis and put it in a file so that we can process it.
-fileName = "../../resources/data/tamil_train.tsv"
+fileName = "../../resources/data/all_records.tsv"
 
 with open (fileName, "r", encoding="UTF-8") as mainFile, open(matchedFn, "w", encoding="UTF-8") as matchedFile, open(unmatchedFn, "w", encoding="UTF-8") as unmatchedFile:
     readTsv = csv.reader(mainFile, delimiter="\t")
     for row in readTsv:
-        txt, emotion = row
-        emojiji = extract_emojis(txt)
-        for em in emojiji:
-            sentiment = get_emoji_sentiment_rank(em)
-            if sentiment == False:
-                if em not in unmatched:
-                    unmatched.append(em)
-            else:
-                if em not in matched:
-                    matched.append(em)
-
-    matchedFile.writelines("\n".join(matched))
-    unmatchedFile.writelines("\n".join(unmatched))
+        if len(row) == 2:
+            txt, emotion = row
+            emojiji = extract_emojis(txt)
+            for em in emojiji:
+                sentiment = get_emoji_sentiment_rank(em)
+                if sentiment == False:
+                    unmatched[(em,emotion)] += 1
+                    occurences[em] += 1
+                else:
+                    matched[(em,emotion)] += sentiment['sentiment_score']
+    for em, tot in occurences.items():
+        pos = unmatched[(em, 'Positive')]
+        neg = unmatched[(em, 'Negative')]
+        neu = tot - (pos +neg)
+        assert(neu>=0)
+        unmatchedFile.write(",".join((em,'-',str(tot),'1',str(neg),str(neu),str(pos),'-','Unknown')) + '\n')
+    matchedFile.writelines("\n".join([elem[0] + ',' + elem[1] + ',' + str(cnt) for elem, cnt in matched.items()]))
+    #unmatchedFile.writelines("\n".join([elem[0] + ',' + elem[1] + ',' + str(cnt) for elem, cnt in unmatched.items()]))
 sys.exit()
 '''
 lineNum = 626
